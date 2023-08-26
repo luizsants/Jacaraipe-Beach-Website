ee1672effaa38322c90fdda0568827746f8297c0
luizsants <100363551+luizsants@users.noreply.github.com>
2023-08-26 13:10:44 -0300

    js actived

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// function showAlert( ){
-//     alert("Bem vindo a Praia de Jacaraípe!😄")
-// }
-// window.onload = showAlert
+function showAlert( ){
+    alert("Bem vindo a Praia de Jacaraípe!😄")
+}
+window.onload = showAlert
